ab08e46b3d7fcfaf2a50b7e836ca37d6212bf424
michaelrodrigue <scottlovos503@gmail.com>
2020-06-17 18:10:30 -0600

    documentacion

--- Private/Modulos/direcciones/procesos.php
+++ Private/Modulos/direcciones/procesos.php
@@ -1,6 +1,15 @@
 <?php 
 
+/**
+  * @author Michael Rodriguez <scottlovos503@gmail.com>
+ */
+
+/** Iniciar una nueva sesión o reanudar la existente */
 session_start();
+
+/**
+ * Incluye implementacion de la configuracion de conexion a la Base de Datos
+*/
 include("../../Config/Config.php");
 $direccion = new direccion($Conexion);
 
@@ -8,20 +17,47 @@ $proceso = '';
 if( isset($_GET['proceso']) && strlen($_GET['proceso'])>0 ){
 	$proceso = $_GET['proceso'];
 }
+/** @global $direccion Se le asigna los datos  */
 $direccion->$proceso( $_GET['direction'] );
 print_r(json_encode($direccion->respuesta));
 
+
+/**
+ * @class direccion
+ */
 class direccion{
     private $datos = array(), $db;
     public $respuesta = ['msg'=>'correcto'];
     
+
+    /**
+     * constructor 
+     * @access public
+     * @function __construct
+     * @param String $db contiene el nombre de la base de datos
+    */
     public function __construct($db){
         $this->db=$db;
     }
+
+
+    /**
+	  * @access public
+     * @function recibirDatos recibe los datos del producto desde el formulario
+     * @param object  $direccion representa los datos en si
+     */
     public function recibirDatos($direccion){
         $this->datos = json_decode($direccion, true);
-       $this->validar();
+        $this->validar();
     }
+
+
+
+    /**
+     * Evalua si los datos recibidos desde el formulario no esten vacios
+     * @access private
+     * @function validar
+     */
     private function validar(){
         if(empty(trim($this->datos['Direccion']))){
             $this->respuesta['msg']='por favor ingrese la Dirección';      
@@ -30,7 +66,14 @@ class direccion{
         }
         $this->almacenar_direccion();
     }
-   
+
+
+    /**
+     * Inserta datos en la Base de datos
+     * @access private
+     * @function almacenar_direccion
+     * @return respuesta
+     */
     private function almacenar_direccion(){
         if( $this->respuesta['msg']==='correcto' ){
             if( $this->datos['accion']==='nuevo' ){
@@ -52,17 +95,27 @@ class direccion{
         }
     }
 
-    
 
-  
-    
 
- 
+    /**
+     * Trae el identificador segun el del usuario logueado
+     * @access public
+     * @function idlogueo
+     * @return respuesta retorna un menaje de exito
+     */
     public function idlogueo($valor='')   {
         $this->db->consultas('SELECT usuario.idusuario from usuario where idusuario="'.$_SESSION['usuario'].'"');
-      return  $this->respuesta = $this->db->obtener_datos();
+        return  $this->respuesta = $this->db->obtener_datos();
     }
 
+
+
+    /**
+     * Elimina un registro e la Base de datos 
+     * @access public
+     * @function deleteDireccion
+     * @return respuesta retorna un mensaje de exito
+     */
     public function deleteDireccion($idDireccion=0){
         $this->db->consultas('
         DELETE FROM direcciones WHERE direcciones.idDireccion ="'.$idDireccion.'" 
@@ -71,19 +124,28 @@ class direccion{
     }
     
 
-
+    /**
+	  * @access public
+     * @function recibirDatos recibe los datos de la direccion desde el formulario
+     * @param object  $direccion representa los datos en si
+    */
     public function	recibirdireccionView($direccion){
         $this->datos = json_decode($direccion, true);
         $this->mostrardirecciones();
     }
+
+
+    /**
+     * Ejecuta una consulta para traer las direcciones que esten en base al dato que esta en la variable session
+     * @access private
+     * @function mostrardirecciones
+     * @return respuesta retorna las direcciones
+     */
     private function mostrardirecciones(){
         $this->db->consultas('SELECT direcciones.* from direcciones JOIN usuario where usuario.idusuario=direcciones.fkUsuario and usuario.idusuario="'.$_SESSION['usuario'].'" ');
         return  $this->respuesta = $this->db->obtener_datos();
-   
     }
     
-		
-		
 }
 
 
