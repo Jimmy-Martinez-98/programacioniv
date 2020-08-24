b0937f475325f8245af183a1601e87ec4e646851
michaelrodrigue <scottlovos503@gmail.com>
2020-08-23 18:24:00 -0600

    chat directo ok

--- public/vistas/chat/chat.js
+++ public/vistas/chat/chat.js
@@ -29,7 +29,6 @@ appChat = new Vue({
 
       this.chatHistory();
     },
-   
   },
   watch: {
     allMessages: function (val) {
@@ -100,5 +99,57 @@ appChat = new Vue({
           .then((this.mensajes.msg = ""));
       }
     },
+    obtenerImagen(e) {
+      let file = e.target.files[0];
+      let upload = storage
+        .ref()
+        .child("imageChat/" + file.name)
+        .put(file);
+      upload.on(
+        "state_changed",
+        (snapshot) => {
+          //muestra el progreso
+          let progress = Math.round(
+            (snapshot.bytesTransferred * 100) / snapshot.totalBytes
+          );
+         
+          document.getElementById("target").innerHTML = `
+          <div class="progress">
+             <div class="progress-bar bg-dark" role="progressbar" style="width:${progress}%;" aria-valuenow="25" aria-valuemin="25" aria-valuemax="100">${progress}%</div>
+          </div>`;
+        },
+
+        (error) => {
+          //muestra error
+          swal.fire({
+            title: "Ups..",
+            text: "Ocurrio un error al cargar Imagen",
+            icon: "error",
+          });
+        },
+        () => {
+          //cuando la imagen ya document.getElementById("target").style.display='none' esta subida
+
+          upload.snapshot.ref.getDownloadURL().then(function (downloadURL) {
+            firebaseDB
+              .ref("/chat")
+              .push({
+                De: appChat.mensajes.de,
+                Para: appChat.mensajes.para,
+                Mensaje: "",
+                imagenMensaje: downloadURL,
+              })
+              .then(
+                ()=>{
+                  document.getElementById("target").style.display = "none"
+                 
+                }
+               
+              );
+          });
+        }
+      );
+    },
+    
   },
 });
