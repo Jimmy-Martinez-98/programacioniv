fd754b6cbd2ac063c129099889439a148f34828d
michaelrodrigue <scottlovos503@gmail.com>
2020-08-26 22:00:51 -0600

    tengo errores

--- public/vistasPerfil/inventario/misproductos.js
+++ public/vistasPerfil/inventario/misproductos.js
@@ -1,11 +1,10 @@
-/**
+/*
  * @author Michael Rodriguez <scottlovos503@gmail.com>
  * @file misproductos.js-> Sirve para la configuracion de los productos
  * @license MIT Libre disttribucion
  * @instance objeto de instancia de Vue.js
  */
-var firebaseDB = firebase.database();
-var Authf = firebase.auth();
+
 var misproductosapp = new Vue({
   el: "#misprod",
   data: {
@@ -16,27 +15,70 @@ var misproductosapp = new Vue({
       quintal: false,
       unidad: false,
       categoria: false,
-      codeProducto: false,
-      descProducto: false,
-      existencias: false,
-      fechaSubida: false,
-      idProducto: false,
-      idUsuario: false,
-      imagen: false,
+      codeProducto: "",
+      descProducto: "",
+      existencias: "",
+      fechaSubida: "",
+      idProducto: "",
+      idUsuario: "",
+      imagen: "",
       libra: false,
-      nombreCooperativa: false,
-      nombreProducto: false,
-      nombreU: false,
-      precio: false,
-      precioVenta: false,
+      nombreCooperativa: "",
+      nombreProducto: "",
+      nombreU: "",
+      precio: "",
+      precioVenta: "",
     },
   },
 
   methods: {
-    // toggleEdit: function () {
-    //   document.getElementById("mod").setAttribute("hidden", true);
-    //   document.getElementById("confirmar").removeAttribute("hidden");
-    // },
+    toggleEdit: function () {
+      document.getElementById("mod").setAttribute("hidden", true);
+      document.getElementById("confirmar").removeAttribute("hidden");
+
+      $("#confirmar").click(function (e) {
+        e.preventDefault();
+
+        firebaseDB
+          .ref("/productos")
+          .set({
+            codeProducto: misproductosapp.modificacion.codeProducto,
+            nombreProducto: misproductosapp.modificacion.nombreProducto,
+            descProducto: misproductosapp.modificacion.descProducto,
+            existencias: misproductosapp.modificacion.existencias,
+            categoria: misproductosapp.modificacion.categoria,
+            Unidad: misproductosapp.modificacion.unidad,
+            libra: misproductosapp.modificacion.libra,
+            Arroba: misproductosapp.modificacion.arroba,
+            Quintal: misproductosapp.modificacion.quintal,
+            Caja: misproductosapp.modificacion.caja,
+          },()=>{
+            misproductosapp.openNotificacion(
+              "succes",
+              "Listo!!",
+              "Tu Producto Se Modifico Exitosamente :)"
+            );
+          })
+        
+          .catch(() => {
+            this.openNotificacion(
+              "danger",
+              "Error!!!",
+              "Tu Producto No Se Ha Podido Modificar :("
+            );
+          });
+      });
+    },
+    openNotificacion: function (color, title, text) {
+      const noti = this.$vs.notification({
+        color,
+        progress: "auto",
+        position: "top-rigth",
+        title,
+        text,
+      });
+      return noti
+    },
     editar: function (id) {
       document.getElementById("code").removeAttribute("hidden");
       document.getElementById("name").removeAttribute("hidden");
@@ -54,6 +96,16 @@ var misproductosapp = new Vue({
       this.modificacion.arroba = id.Arroba;
       this.modificacion.quintal = id.Quintal;
       this.modificacion.caja = id.Caja;
+      this.modificacion.fechaSubida = id.fechaSubida;
+      this.modificacion.idProducto = id.idProducto;
+      this.modificacion.idUsuario = firebaseAuth.currentUser.uid;
+      this.modificacion.imagen = id.imagen;
+      this.modificacion.nombreCooperativa = id.nombreCooperativa;
+      this.modificacion.nombreProducto = id.nombreProducto;
+      this.modificacion.nombreU = id.nombreU;
+      this.modificacion.precio = id.precio;
+      this.modificacion.precioVenta = id.precioVenta;
+
       document.getElementById("ventasT").setAttribute("hidden", false);
       document.getElementById("selectVentas").removeAttribute("hidden");
     },
@@ -66,8 +118,8 @@ var misproductosapp = new Vue({
       var user = firebase.auth().currentUser;
       let dbchild = firebaseDB.ref("Productos/");
       if (user) {
+        let todoProducto = [];
         dbchild.on("value", (snapshot) => {
-          let todoProducto = [];
           snapshot.forEach((element) => {
             if (user.uid === element.val().idUsuario) {
               todoProducto.push(element.val());
@@ -79,37 +131,38 @@ var misproductosapp = new Vue({
         // No user is signed in.
       }
     },
-    deleteproducto: function (id) {
-      Swal.fire({
-        title: "Estas seguro?",
-        text: "No podras revertir esto",
-        icon: "warning",
-        showCancelButton: true,
-        confirmButtonColor: "#3085d6",
-        cancelButtonColor: "#d33",
-        confirmButtonText: "Si, Eliminalo!",
-      }).then((result) => {
-        if (result.value) {
-          firebaseDB
-            .ref("Productos/" + id)
-            .remove()
-            .then(() => {
-              swal.fire({
-                title: "Eliminado",
-                text: "Producto Eliminado",
-                icon: "success",
-              });
-            })
-            .catch((error) => {
-              swal.fire({
-                title: "Ups..",
-                text: "Ocurrio un error inesperdado",
-                icon: "error",
-              });
-            });
-        }
-      });
-    },
+
+    //     deleteproducto: function (id) {
+    //       Swal.fire({
+    //         title: "Estas seguro?",
+    //         text: "No podras revertir esto",
+    //         icon: "warning",
+    //         showCancelButton: true,
+    //         confirmButtonColor: "#3085d6",
+    //         cancelButtonColor: "#d33",
+    //         confirmButtonText: "Si, Eliminalo!",
+    //       }).then((result) => {
+    //         if (result.value) {
+    //           firebaseDB
+    //             .ref("Productos/" + id)
+    //             .remove()
+    //             .then(() => {
+    //               swal.fire({
+    //                 title: "Eliminado",
+    //                 text: "Producto Eliminado",
+    //                 icon: "success",
+    //               });
+    //             })
+    //             .catch((error) => {
+    //               swal.fire({
+    //                 title: "Ups..",
+    //                 text: "Ocurrio un error inesperdado",
+    //                 icon: "error",
+    //               });
+    //             });
+    //         }
+    //       });
+    //  },
   },
   created: function () {
     this.productosmios();
