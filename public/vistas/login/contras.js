7a1118992e9a1e6f13ef43f790746ae18e448ccd
michaelrodrigue <scottlovos503@gmail.com>
2020-10-02 12:02:07 -0600

    ...

--- public/vistas/login/contras.js
+++ public/vistas/login/contras.js
@@ -23,37 +23,37 @@ var appcontras = new Vue({
       email = this.resetPassword.correo;
       firebaseAuth.sendPasswordResetEmail(email)
         .then(() => {
-        swal
-          .fire({
-            title: "Restablecimiento de Contraseña",
-            text: "Se ha enviado un mensaje a su correo para que actualize su contraseña",
-            icon: "info",
-            buttonStyling: true,
-            showCloseButton: true,
-            closeButtonArialLabel: "cerrar alerta",
-            position: "top",
-            allowOutsideClick: false,
-            allowEscapeKey: false,
-          })
-          .then(() => {
-            location.href = "login.html";
-          });
-      }).catch((error) => {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        console.log(errorCode, "=>", errorMessage);
-        if (errorCode == "auth/user-not-found") {
-          document.getElementById(
-            "alerta"
-          ).innerHTML = `<div class="alert alert-warning alert-dismissible fade show" role="alert">
+          swal
+            .fire({
+              title: "Restablecimiento de Contraseña",
+              text: "Se ha enviado un mensaje a su correo para que actualize su contraseña",
+              icon: "info",
+              buttonStyling: true,
+              showCloseButton: true,
+              closeButtonArialLabel: "cerrar alerta",
+              position: "top",
+              allowOutsideClick: false,
+              allowEscapeKey: false,
+            })
+            .then(() => {
+              location.href = "login.html";
+            });
+        }).catch((error) => {
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          console.log(errorCode, "=>", errorMessage);
+          if (errorCode == "auth/user-not-found") {
+            document.getElementById(
+              "alerta"
+            ).innerHTML = `<div class="alert alert-warning alert-dismissible fade show" role="alert">
                       El Usuario con este correo no existe en nuestra base de datos 
                       <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                       </button>
                     </div>`;
-        }
+          }
 
-      });
+        });
     },
 
     /**
