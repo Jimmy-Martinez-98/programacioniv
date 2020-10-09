c306b2a8622cbfe45f6c8c73491b934472071d0a
michaelrodrigue <scottlovos503@gmail.com>
2020-10-09 11:13:51 -0600

    arreglo de paginacion

--- public/vistasPerfil/gestionFacturas/js/gestionF.js
+++ public/vistasPerfil/gestionFacturas/js/gestionF.js
@@ -5,64 +5,71 @@
  * @instance objeto de instancia de Vue.js
  */
 var factura = new Vue({
-    el: '#factura',
+    el: "#factura",
     data: {
-        OwnerOfOrder: '',
+        OwnerOfOrder: "",
         datos: [],
         forPage: 6,
         pages: [],
-        page: 1
+        page: 1,
     },
     /**
      * Esta funcion llama a metodos para ejecutarlos al cargar el DOM
-     * @access public 
-     * @function created 
+     * @access public
+     * @function created
      */
     created: function () {
         this.observador();
-        this.datos = []
+
     },
 
     computed: {
         displayFacturas: function () {
             return this.paginate(this.datos);
+        },
+        updateData: function () {
+            this.observador()
         }
     },
-    weatch: {
+    watch: {
+        updateData() {},
         datos() {
-            this.setFacturas();
-        }
+            return this.setFacturas();
+        },
+
     },
     methods: {
         /**
          * observa el estado de la session en la web
-         * @access public 
+         * @access public
          * @function observador
          */
         async observador() {
             await firebaseAuth.onAuthStateChanged((user) => {
                 if (user) {
-                    firebaseDB.ref('dataFacturas').on('value', snap => {
-                        this.datos = []
-                        snap.forEach(element => {
+                    firebaseDB.ref("dataFacturas").on("value", (snap) => {
+                        this.datos = [];
+                        snap.forEach((element) => {
                             if (element.val().idOwner == user.uid) {
-                                this.traerUserOrder(element.val().idOwnerOfOrder)
-                                this.datos.push(element.val())
+                                this.traerUserOrder(element.val().idOwnerOfOrder);
+                                this.datos.push(element.val());
                             }
                         });
-                    })
+                    });
 
                 }
-            })
+            });
+
 
         },
 
         /**
-         * Muestra el numero de paginas 
+         * Muestra el numero de paginas
          * @access public
          * @function setFacturas
          */
         setFacturas: function () {
+          
             let numberOfPage = Math.ceil(this.datos.length / this.forPage);
             this.pages = [];
             for (let i = 1; i <= numberOfPage; i++) {
@@ -71,75 +78,82 @@ var factura = new Vue({
         },
         /**
          * Calcula la el numero de paginas para la tabla
-         * @access public 
+         * @access public
          * @function paginate
          * @param {object} facturas -> representa el total de facturas de la db
          */
         paginate: function (facturas) {
             let page = this.page,
                 forPage = this.forPage,
-                from = (page * forPage) - forPage,
-                to = (pages * forPage),
-                return facturas.slice(from, to);
+                from = page * forPage - forPage,
+                to = pages * forPage;
+            return facturas.slice(from, to);
         },
-
-
 
         /**
          * obtiene los datos del usuario de la BD para mostrar nombre en la vista
-         * @access public   
+         * @access public
          * @function traerUserOrder
-         * @param {String} user 
+         * @param {String} user
          */
         traerUserOrder: function (user) {
-            firebaseDB.ref('users/').orderByChild('uId').equalTo(user).on('value', snap => {
-                snap.forEach(element => {
-                    if (user == element.val().uId) {
-                        this.OwnerOfOrder = element.val().nombreUsuario
-                    }
-
+            firebaseDB
+                .ref("users/")
+                .orderByChild("uId")
+                .equalTo(user)
+                .on("value", (snap) => {
+                    snap.forEach((element) => {
+                        if (user == element.val().uId) {
+                            this.OwnerOfOrder = element.val().nombreUsuario;
+                        }
+                    });
                 });
-            })
         },
         /**
-         * es cuando el usuario clickea el item verificar del menu de opciones en la tabla 
+         * es cuando el usuario clickea el item verificar del menu de opciones en la tabla
          * @access public
          * @function verificar
-         * @param {String} id 
+         * @param {String} id
          */
         verificar: function (id) {
-            firebaseDB.ref('dataFacturas/' + id).update({
-                'estado': "Verificada"
-            }).catch(() => {
-                this.$vs.notification({
-                    square: true,
-                    progress: "auto",
-                    color: 'danger',
-                    title: "Ocurrio un error al intentar realizar la acción",
-                    position: 'bottom-center',
-                    icon: "<i class='bx error-circle'></i>"
+            firebaseDB
+                .ref("dataFacturas/" + id)
+                .update({
+                    estado: "Verificada",
+                })
+                .catch(() => {
+                    this.$vs.notification({
+                        square: true,
+                        progress: "auto",
+                        color: "danger",
+                        title: "Ocurrio un error al intentar realizar la acción",
+                        position: "bottom-center",
+                        icon: "<i class='bx error-circle'></i>",
+                    });
                 });
-            })
         },
         /**
-         * es cuando el usuario clickea el item anular del menu de opciones en la tabla 
+         * es cuando el usuario clickea el item anular del menu de opciones en la tabla
          * @access public
          * @function anular
-         * @param {String} id 
+         * @param {String} id
          */
         anular: function (id) {
-            firebaseDB.ref('dataFacturas/' + id).update({
-                'estado': "Anulada"
-            }).catch(() => {
-                this.$vs.notification({
-                    square: true,
-                    progress: "auto",
-                    color: 'danger',
-                    title: "Ocurrio un error al intentar realizar la acción",
-                    position: 'bottom-center',
-                    icon: "<i class='bx error-circle'></i>"
+            firebaseDB
+                .ref("dataFacturas/" + id)
+                .update({
+                    estado: "Anulada",
+                })
+                .catch(() => {
+                    this.$vs.notification({
+                        square: true,
+                        progress: "auto",
+                        color: "danger",
+                        title: "Ocurrio un error al intentar realizar la acción",
+                        position: "bottom-center",
+                        icon: "<i class='bx error-circle'></i>",
+                    });
                 });
-            })
-        }
-    }
-})
+        },
+    },
+});
